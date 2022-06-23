d23ad0c5831274b59a2a9d1045c93d5963377043
robert.makyla <robert.makyla@gmail.com>
2022-06-23 11:28:49 +0200

    actor typed - async messaging

---
+++ src/main/scala/akkaplayground/actor/AsyncActor.scala
@@ -0,0 +1,36 @@
+package akkaplayground.actor
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+
+/**
+ * 2 players - each one of then is sending messages to himself
+ */
+object AsyncActor {
+
+  case class Msg(iteration: Int, actorName: String)
+
+  def player: Behavior[Msg] = Behaviors.receive { (ctx, msg) =>
+    msg match {
+      case Msg(i, actorName) =>
+        ctx.log.info(s"${i} at ${actorName}")
+        ctx.self ! Msg(i + 1, actorName)
+        Behaviors.same
+    }
+  }
+
+  val main: Behavior[Any] = Behaviors.setup { ctx => // All this will be done when actor is created, not when message is received
+    val player1 = ctx.spawn(player, "Mike")
+    val player2 = ctx.spawn(player, "John")
+    player1 ! Msg(0, "Mike")
+    player2 ! Msg(0, "John")
+    Behaviors.same
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val mainRef = ActorSystem(main, "main")
+    Thread.sleep(10)
+    mainRef.terminate()
+  }
+}
