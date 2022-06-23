a1b8ed86963a12b774167aaf2f5e546da3fa694f
robert.makyla <robert.makyla@gmail.com>
2022-06-23 11:20:04 +0200

    actor typed - sync messaging

---
+++ src/main/scala/akkaplayground/actor/SyncActor.scala
@@ -0,0 +1,42 @@
+package akkaplayground.actor
+
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+
+object SyncActor {
+
+  trait PingPong
+  case class Ping(iteration: Int = 0, replyTo: ActorRef[PingPong]) extends PingPong
+  case class Pong(iteration: Int = 0, replyTo: ActorRef[PingPong]) extends PingPong
+
+  val delayMillis = 100
+
+  def player: Behavior[PingPong] = Behaviors.receive { (ctx, msg) =>
+    msg match {
+      case Ping(i, replyTo) =>
+        ctx.log.info(s" -- ping --> ${i}")
+        Thread.sleep(delayMillis)
+        replyTo ! Pong(i + 1, ctx.self)
+        Behaviors.same
+      case Pong(i, replyTo) =>
+        ctx.log.info(s" <-- pong -- ${i}")
+        Thread.sleep(delayMillis)
+        replyTo ! Ping(i + 1, ctx.self)
+        Behaviors.same
+    }
+  }
+
+  val main: Behavior[Any] = Behaviors.setup { ctx => // All this will be done when actor is created, not when message is received
+    val player1 = ctx.spawn(player, "player-1")
+    val player2 = ctx.spawn(player, "player-2")
+    player1 ! Ping(0, player2)
+    Behaviors.same
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val mainRef = ActorSystem(main, "main")
+    Thread.sleep(1000)
+    mainRef.terminate()
+  }
+}
